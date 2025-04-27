46857de47ec18e5381f47f0922fad444cb7a7d77
JiantongChen <jiantongchen1123@163.com>
2025-04-27 09:54:03 +0800

    sync

--- scripts/cjt/calvin_abcd_d/slurm_eval.sh
+++ scripts/cjt/calvin_abcd_d/slurm_eval.sh
@@ -8,7 +8,7 @@ calvin_conf_path="calvin/calvin_models/conf"
 vit_checkpoint_path="checkpoints/vit_mae/mae_pretrain_vit_base.pth" # downloaded from https://drive.google.com/file/d/1bSsvRI4mDM3Gg51C6xO0l9CbojYw3OEt/view?usp=sharing
 save_checkpoint_path="checkpoints/"
 ### NEED TO CHANGE the checkpoint path ###
-resume_from_checkpoint="checkpoints/seer_calvin/official/finetune/15.pth"
+resume_from_checkpoint="checkpoints/seer_calvin/finetune_abcd_d_ep5/19.pth"
 IFS='/' read -ra path_parts <<< "$resume_from_checkpoint"
 run_name="${path_parts[-2]}"
 log_name="${path_parts[-1]}"
